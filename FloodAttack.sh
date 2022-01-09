3fef736132900aa064c0aa62ce21e877865c3202
Matthieu Prezut <jesa974@gmail.com>
2022-01-10 00:00:58 +0100

    Ajout de la fuite du fichier

--- FloodAttack.sh
+++ FloodAttack.sh
@@ -6,22 +6,43 @@ MAX_FIL=6
 NAME="TEST"
 RND=0
 PATH=`pwd`
+arrMyFile=("/home/oem/Documents/RedTeam/myFile.txt" "0")
+declare -a arr
 
 gen_random_string() {
-	NAME=`echo $RANDOM | md5sum | head -c 20; echo;`
+	NAME=`echo $RANDOM | /usr/bin/md5sum | /usr/bin/head -c 20; echo;`
 }
 
 gen_random_number() {
-	RND=`echo $((0 + $RANDOM % 2))`
+	RND=`echo $(($1 + $RANDOM % $2))`
 }
 
-
-for i in $(seq 0 $MAX_DIR)
+for ((i=0;i<=$MAX_DIR;i+=1))
 do
-	#gen_random_string()
-	#gen_random_number()
-	NAME_FILE="$NAME$i"
+	gen_random_string
+	arr+=( "$PATH/$NAME" )
+	echo ${arr[$i]}
 	FINAL_PATH="$PATH/"
-	DIR_PATH="$FINAL_PATH$NAME_FILE"
+	DIR_PATH="$FINAL_PATH$NAME"
 	/bin/mkdir $DIR_PATH
+done
+
+/usr/bin/mv ${arrMyFile[0]} ${arr[0]}
+arrMyFile[0]="${arr[0]}/myFile.txt"
+
+TAILLE=`echo ${#arr[@]}`
+
+while true
+do
+	gen_random_number 0 $TAILLE
+	while (($RND == ${arrMyFile[1]}))
+	do
+		gen_random_number 0 $TAILLE
+	done
+	
+	/usr/bin/mv ${arrMyFile[0]} ${arr[$RND]} > /dev/null 2>&1
+	arrMyFile[0]="${arr[$RND]}/myFile.txt"
+	arrMyFile[1]=$RND
+
+	/usr/bin/sleep 10
 done
